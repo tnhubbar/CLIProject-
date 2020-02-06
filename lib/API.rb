f9e604458a39c382ec02f5d31034be660ecbeee8
tnhubbar <taryn.johnson919@gmail.com>
2020-02-06 18:17:22 -0500

    added HTTParty and removed unirest gem

--- lib/API.rb
+++ lib/API.rb
@@ -1,6 +1,15 @@
+
+
 class CLIProject::API
 
-  BASE = " https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/mealplans/generate"
+
+  def self.get_recipes
+  response = HTTParty.get "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/mealplans/generate?targetCalories=2000&timeFrame=day",
+  headers:{
+    "X-RapidAPI-Host" => "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
+    "X-RapidAPI-Key" => "c1316768abmsh89f8e89879289f6p16f69ejsn8a84bbc79579"
+  }
+end 
 
    #def get_recipes(input)
      #input = 2000
--- lib/recipe_controller.rb
+++ lib/recipe_controller.rb
@@ -7,8 +7,9 @@ end
 def run 
     puts "Welcome to the Recipe Picker app"
     puts "How many calories would you like to consume per day?"
-    calories = gets.chomp
-    input = #{calories} per day  
+    #calories = gets.chomp
+    #input = #{calories} per day 
+    CLIProject::API.get_recipes 
 end 
 
 
