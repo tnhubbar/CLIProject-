7b3c2745674d609d989131a6fde7b117718d5019
tnhubbar <taryn.johnson919@gmail.com>
2020-02-06 10:37:37 -0500

    Added requesting link for api

---
+++ lib/API.rb
@@ -0,0 +1,9 @@
+class CLIProject::API
+
+  BASE = " https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/mealplans/generate"
+
+   #def get_recipes(input)
+    #HTTParty.get(BASE+"recipes/#{input}")
+   #end 
+
+end 
--- lib/recipe_controller.rb
+++ lib/recipe_controller.rb
@@ -6,6 +6,8 @@ end
 
 def run 
     puts "Welcome to the Recipe Picker app"
+    puts "How many calories would you like to consume per day?"
+    calories = gets.chomp 
 end 
 
 
