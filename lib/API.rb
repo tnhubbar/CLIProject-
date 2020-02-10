198a5cc07f32a3688132803129be74f49d845dea
tnhubbar <taryn.johnson919@gmail.com>
2020-02-10 14:14:38 -0500

    interpolated user input into API request.

--- lib/API.rb
+++ lib/API.rb
@@ -3,8 +3,8 @@
 class CLIProject::API
 
 
-  def self.get_pairings_for_wine
-  HTTParty.get "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/food/wine/dishes?wine=merlot",
+  def self.get_pairings_for_wine(wine_flavor)
+  HTTParty.get "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/food/wine/dishes?wine=#{wine_flavor}",
   headers:{
     "X-RapidAPI-Host" => "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
     "X-RapidAPI-Key" => "c1316768abmsh89f8e89879289f6p16f69ejsn8a84bbc79579"
--- lib/recipe_controller.rb
+++ lib/recipe_controller.rb
@@ -6,13 +6,14 @@ def initialize
 end 
 
 def run 
-    #puts "Welcome to the meal plan generator app"
+    #puts "Welcome to the wine pairings app! We will make sure your wine is paired for romance tonight. "
     #puts "How many calories would you like to consume per day?"
     #puts "Your choices are 1000, 1500, 2000, or 2500."
-    #calories = gets.strip
-   api_response = CLIProject::API.get_recipes_for_1000 
+    #wine_flavor = gets.strip
+   api_response = CLIProject::API.get_pairings_for_wine 
    binding.pry
    recipe_instance = CLIProject::Recipes.new(api_response)
+   api_response["meals"]["title"] 
    #puts "Is this meal plan one you'd be interested in?"
    #puts "Please type yes or no."
    #answer = gets.strip 
